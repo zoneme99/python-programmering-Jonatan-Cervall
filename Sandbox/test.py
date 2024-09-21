387680c6e23425e79ce700ea6fda37642dc13000
zoneme99 <jonatancervall@gmail.com>
2024-09-21 16:55:45 +0200

    klar med sista uppgift i 08

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,3 +1,10 @@
-y = [1,2,3]
-y.insert(0,[6,7,8])
-print(y)
+# import numpy as np
+
+# array = ['1.2','2.0','3.2']
+# f = np.vectorize(float)
+# print(f(array))
+from collections import Counter
+b = [2,2,2,2,2,5,5,4,4,6,6,1,1,8,8,9,7]
+a = Counter(b)
+a = sorted(a)
+print(a)
