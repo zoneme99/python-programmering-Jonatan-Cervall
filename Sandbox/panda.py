6b40bfba88b398931dc2c3bb1b5ce919cb48a986
zoneme99 <jonatancervall@gmail.com>
2024-09-24 12:55:57 +0200

    testa vectorize om inte använda np.map på labb2

--- Sandbox/panda.py
+++ Sandbox/panda.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('Sandbox/all_weekly_excess_deaths.csv')
 
-df["covid_deaths"].plot()
-df["non_covid_deaths"].plot()
+df["covid_deaths"].plot(color = "red")
+df["non_covid_deaths"].plot(color = "green")
 
 plt.show()
--- Sandbox/test.py
+++ Sandbox/test.py
@@ -3,8 +3,17 @@
 # array = ['1.2','2.0','3.2']
 # f = np.vectorize(float)
 # print(f(array))
-from collections import Counter
-b = [2,2,2,2,2,5,5,4,4,6,6,1,1,8,8,9,7]
-a = Counter(b)
-a = sorted(a)
-print(a)
+
+# from collections import Counter
+# b = [2,2,2,2,2,5,5,4,4,6,6,1,1,8,8,9,7]
+# a = Counter(b)
+# a = sorted(a)
+# print(a)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(10)
+y = np.multiply(np.multiply(2,np.log(x)), np.pow(x, 2)) 
+plt.plot(x,y)
+plt.show()
