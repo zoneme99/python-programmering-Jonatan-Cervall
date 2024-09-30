a1aa430ef9e3ade0ee4dbaca71f7100cef430d69
Jonatan Cervall <jonatancervall@gmail.com>
2024-09-30 14:11:09 +0200

    data_seperation done

--- Labs/Labb2/bonus.py
+++ Labs/Labb2/bonus.py
@@ -6,16 +6,27 @@ df = pd.read_csv("Labs/Labb2/datapoints.csv")
 
 df = df.sample(frac = 1)
 
-find = 25
+find = 50
 
 #print(df["(0-pichu 1-pikachu)"][3])
 
-def find_pokemon_index(dataseries, pokemon, to_find):
-    pokemon_indices = list()
-    for index in dataseries.index:
-        if dataseries[index] == pokemon:
-            pokemon_indices.append(index)
-    return pokemon_indices
+def data_seperation(dataframe, to_find):
+    test_pokemons = pd.DataFrame()
+    train_pokemons = pd.DataFrame()
+    pikachu = 0
+    pichu = 0
+    for index, row in dataframe.iterrows():
+        if pikachu < (to_find / 2):
+            test_pokemons = pd.concat([test_pokemons, row.to_frame().T])
+            pikachu += 1
+        elif pichu < (to_find / 2):
+            test_pokemons = pd.concat([test_pokemons, row.to_frame().T])
+            pichu += 1
+        else:
+            train_pokemons = pd.concat([train_pokemons, row.to_frame().T])
+    return test_pokemons, train_pokemons
 
-lst = find_pokemon_index(df["(0-pichu 1-pikachu)"], 1, 10)
-print(len(lst))
+test, train = data_seperation(df, find)
+
+print(test)
+print(len(train))
--- Sandbox/test.py
+++ Sandbox/test.py
@@ -8,5 +8,9 @@ y = pd.Series(np.array([20,10,40]), index = [x for x in range(3)])
 
 df = pd.DataFrame({'Column1': x, 'Column2': y})
 df = df.sort_values("Column2")
-for poke in df.index:
-    print(df["Column2"][poke])
+ff = pd.DataFrame()
+for index, row in df.iterrows():
+    ff = pd.concat([ff, row.to_frame().T])
+    
+print(ff)
+ 
