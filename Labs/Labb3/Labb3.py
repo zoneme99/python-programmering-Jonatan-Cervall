353c1b6b5c94bd33528ba6a426aece603227dab9
Jonatan Cervall <jonatancervall@gmail.com>
2024-10-07 11:57:25 +0200

    fixat left/right

--- Labs/Labb3/Labb3.py
+++ Labs/Labb3/Labb3.py
@@ -6,6 +6,8 @@ class line:
         self.k = k
         self.m = m
         self.df = self.get_labels(path_to_data)
+        self.left = self.df[self.df["label"] == 0]
+        self.right = self.df[self.df["label"] == 1]
 
     def classify(self, k, m, x, y):
         if k*x + m >= y:
@@ -27,21 +29,19 @@ class line:
             raise ValueError("first and second argument must have same length")
     def save_dataframe(self, path):
         self.df.to_csv(path, index=False)
-    def display(self):
-        left = self.df[self.df["label"] == 0]
-        right = self.df[self.df["label"] == 1]
-        plt.scatter(left["x"],left["y"], color = "orange")
-        plt.scatter(right["x"],right["y"], color = "blue")
-        plt.plot([x for x in range(-5,6)], [self.k*x + self.m for x in range(-5,6)])
+    def display(self, color):
+        plt.scatter(self.left["x"],self.left["y"], color = "orange")
+        plt.scatter(self.right["x"],self.right["y"], color = "blue")
+        plt.plot([x for x in range(-5,6)], [self.k*x + self.m for x in range(-5,6)], color = color)
         plt.xlim(-5,5)
         plt.ylim(-5,5)
         plt.legend(["Beneath Line", "Above Line"])
-        plt.show()
 
 if __name__ == '__main__':
     line1 = line(-1, 0, "Labs/Labb3/unlabelled_data.csv")
     line1.save_dataframe("labelled_data.csv")
-    line1.display()
-
+    print(f"{len(line1.left)}/{len(line1.right)}")
+    line1.display("green")
+    plt.show()
 
 
