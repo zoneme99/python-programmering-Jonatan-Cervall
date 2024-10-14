5b5f7ea537cf4b7bdf53c0105e83dd290ecaddd4
Jonatan Cervall <jonatancervall@gmail.com>
2024-10-14 15:32:22 +0200

    working in test.py

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,4 +1,27 @@
-import random as rnd
 
-lst = [rnd.randint(1,10) % rnd.randint(1,10) for _ in range(10)]
-print(lst)
+
+nums = [153456,123406,124456,323456,123458,123756]
+
+
+def find_key(nums):
+    passcode = list()
+    matrix = list()
+    for serie in nums:
+       matrix.append([int(num) for num in str(serie)])
+
+    counts = dict()
+    for index in range(len(matrix)):
+        if matrix[index][0] not in counts:
+            counts[matrix[index][0]] = 1
+        else:
+            counts[matrix[index][0]] += 1
+            passcode.append(min(counts, key=counts.get))
+            break
+    
+    
+    return passcode
+
+#change column matrix[0][1]
+#change row matrix[1][0]
+#checking every row, same column
+print(find_key(nums))
