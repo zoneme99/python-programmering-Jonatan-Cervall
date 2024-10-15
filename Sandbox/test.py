78e829723fb6bd4b6760a2a0c0356feb62ff14ad
zoneme99 <jonatancervall@gmail.com>
2024-10-15 12:34:57 +0200

    impl. equ in E12 fraction

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,27 +1,8 @@
+nums = {'a' : 2, 'b' : 2, 'c' : 3, 'd' : 1}
 
+print(min(nums, key = nums.get))
+print(min(nums, key = nums.values))
 
-nums = [153456,123406,124456,323456,123458,123756]
+#print(nums.get('a'))
 
-
-def find_key(nums):
-    passcode = list()
-    matrix = list()
-    for serie in nums:
-       matrix.append([int(num) for num in str(serie)])
-
-    counts = dict()
-    for index in range(len(matrix)):
-        if matrix[index][0] not in counts:
-            counts[matrix[index][0]] = 1
-        else:
-            counts[matrix[index][0]] += 1
-            passcode.append(min(counts, key=counts.get))
-            break
-    
-    
-    return passcode
-
-#change column matrix[0][1]
-#change row matrix[1][0]
-#checking every row, same column
-print(find_key(nums))
+#print(min(nums))
