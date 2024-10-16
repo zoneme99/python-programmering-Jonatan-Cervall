b1f24fa3eb3b6abb08077a0bd217064b5f131335
Jonatan Cervall <jonatancervall@gmail.com>
2024-10-16 17:04:18 +0200

    continue working on local modules

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,13 +1,4 @@
-import sys
-print(sys.path)
-exit()
+from ..function_class_saves import functions
 
-import function_class_saves.functions as fu
-
-
-
-num, denom = fu.dec_to_frac(1.25)
-
-print(f"{num}/{denom}")
-
+print(functions.dec_to_frac(1.75))
 
