19a4bf0a4c6efa4f4d5c3c66cc76c0fe205da7bd
Jonatan Cervall <jonatancervall@gmail.com>
2024-10-17 17:18:43 +0200

    derive function not working in pandas.py

--- Sandbox/panda.py
+++ Sandbox/panda.py
@@ -1,9 +1,40 @@
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
-df = pd.read_csv('Sandbox/all_weekly_excess_deaths.csv')
+# df = pd.read_csv('Sandbox/50000_Sales_Records.csv')
+# group = df.groupby('Region')
+# mean_by_region = group['Total Revenue'].mean()
+# df = df.set_index('hej')
+# print(df.head(5))
 
-df["covid_deaths"].plot(color = "red")
-df["non_covid_deaths"].plot(color = "green")
 
-plt.show()
+def derive_function(func):
+    """
+    Derive a function and return the derived function
+    
+    Parameters
+    ----------
+    func : function
+        The function to be derived
+    
+    Returns
+    -------
+    derived_func : function
+        The derived function
+    """
+    def derived_func(x):
+        h = 1e-8
+        return (func(x+h) - func(x-h)) / (2*h)
+    return derived_func
+    
+def func(x):
+    return math.pow(x, 3)
+
+
+func = derive_function(func)
+
+print(func(2))
+print(1e-1)
--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,20 +1,8 @@
 import context
 import function_class_saves.functions as fu
 import matplotlib.pyplot as plt
-
-start = 2
-denom = 50
-
-arr = [1/x for x in range(start,denom)]
-def dec_replace(num):
-    if str(num).split(".")[-1] == '0':
-        return int(num)
-    else:
-        return num
-arr = map(dec_replace, arr)
-arr2 = [len(str(x).split(".")[-1]) if isinstance(x, float) else "" for x in arr]
-x = [x for x in range(start,denom)]
+import numpy as np
 
 
-plt.plot(x,arr2)
-plt.show()
+
+print(np.pi)
