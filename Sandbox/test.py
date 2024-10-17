1701bb21435d1f377f8c4fba604aa64c78f3d0cb
Jonatan Cervall <jonatancervall@gmail.com>
2024-10-17 14:11:05 +0200

    kan nu importera mina modules

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,4 +1,20 @@
-from ..function_class_saves import functions
+import context
+import function_class_saves.functions as fu
+import matplotlib.pyplot as plt
 
-print(functions.dec_to_frac(1.75))
+start = 2
+denom = 50
 
+arr = [1/x for x in range(start,denom)]
+def dec_replace(num):
+    if str(num).split(".")[-1] == '0':
+        return int(num)
+    else:
+        return num
+arr = map(dec_replace, arr)
+arr2 = [len(str(x).split(".")[-1]) if isinstance(x, float) else "" for x in arr]
+x = [x for x in range(start,denom)]
+
+
+plt.plot(x,arr2)
+plt.show()
